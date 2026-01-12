a726be658c8cfd6afc1e1d78b24788278a0d0abe
Grandpa <grandpacreates@gmail.com>
2026-01-12 14:34:15 -0300

    arreglando errores

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "simple-peer": "simple-peer/simplepeer.min.js",
+    },
+  },
   define: {
     global: "globalThis",
   },
